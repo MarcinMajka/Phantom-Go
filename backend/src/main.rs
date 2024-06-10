a17c66cbe8995664affe4ea95b52015eabc8a496
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-06-10 22:03:18 +0200

    Axum tutorial: Added another route and handler

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,35 @@
-use axum::{extract::Query, response::Html, routing::get, Router};
+use axum::{
+    extract::Query,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use rand::{thread_rng, Rng};
 use serde::Deserialize;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("listening on {}", addr);
+    let routes_both = Router::new()
+        .route("/rng", get(handler_rng))
+        .route("/hello", get(handler_hello));
 
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {addr}");
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(routes_both.into_make_service())
         .await
         .unwrap();
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 struct RangeParameters {
     start: usize,
     end: usize,
 }
 
-async fn handler(Query(range): Query<RangeParameters>) -> Html<String> {
+async fn handler_rng(Query(range): Query<RangeParameters>) -> Html<String> {
+    println!("->> {:<12} - handler_hello - {range:?}", "HANDLER");
     let random_number = thread_rng().gen_range(range.start..=range.end);
     let html_template = include_str!("../../index.html");
     let response_html = html_template
@@ -29,4 +37,16 @@ async fn handler(Query(range): Query<RangeParameters>) -> Html<String> {
         .replace("{{end}}", &range.end.to_string())
         .replace("{{random_number}}", &random_number.to_string());
     Html(response_html)
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+    // params.name is Option<String>, as_deref() returns Option<&str>, so we can use unwrap_or() for default value
+    let name = params.name.as_deref().unwrap_or("Wordlasdfdstgr...");
+    Html(format!("Hello <strong>{name}</strong>"))
 }
