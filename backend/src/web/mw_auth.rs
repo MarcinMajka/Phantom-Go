d78a281bee631a11579a8aae6233b1a44095916d
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-06-17 22:38:57 +0200

    Axum tutorial: custom extractor

--- backend/src/web/mw_auth.rs
+++ backend/src/web/mw_auth.rs
@@ -1,7 +1,11 @@
+use axum::extract::FromRequestParts;
+use axum::http::request::Parts;
+use axum::{async_trait, RequestPartsExt};
 use axum::{http::Request, middleware::Next, response::Response};
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
+use crate::ctx::Ctx;
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
@@ -22,6 +26,28 @@ pub async fn mw_require_auth<B>(
     // TODO: Token components validation
 
     Ok(next.run(req).await)
+}
+
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - Ctx", "EXTRACTOR");
+
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+        // Parse token
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthTokenCookie)
+            .and_then(parse_token)?;
+
+        // TODO: Token components validation
+
+        Ok(Ctx::new(user_id))
+    }
 }
 
 fn parse_token(token: String) -> Result<(u64, String, String)> {
