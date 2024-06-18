6652531ced171fb62d7fe3abe9976ad551a0fbb8
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-06-18 22:55:31 +0200

    Axum tutorial: Ctx in mw_auth

--- backend/src/web/mw_auth.rs
+++ backend/src/web/mw_auth.rs
@@ -5,25 +5,19 @@ use axum::{http::Request, middleware::Next, response::Response};
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
-use crate::ctx::Ctx;
+use crate::ctx::{self, Ctx};
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
 
 pub async fn mw_require_auth<B>(
-    cookies: Cookies,
+    ctx: Result<Ctx>,
     req: Request<B>,
     next: Next<B>,
 ) -> Result<Response> {
     println!("->> {:<12} - mw_require_auth", "MIDDLEWARE");
 
-    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
-
-    // Parse token
-    let (user_id, exp, sign) = auth_token
-        .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
-
-    // TODO: Token components validation
+    // ctx has been extracted with an extractor, which means now we can use this extractor in all of the routes
+    ctx?;
 
     Ok(next.run(req).await)
 }
