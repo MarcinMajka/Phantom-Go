88a54ad375c503c08e296895b96a134a0dbc4f40
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-06-19 17:53:44 +0200

    Axum tutorial: Ctx in the REST APIs

--- backend/src/web/routes_tickets.rs
+++ backend/src/web/routes_tickets.rs
@@ -4,8 +4,11 @@ use axum::{
     Json, Router,
 };
 
-use crate::model::{ModelController, Ticket, TicketForCreate};
 use crate::Result;
+use crate::{
+    ctx::Ctx,
+    model::{ModelController, Ticket, TicketForCreate},
+};
 
 pub fn routes(mc: ModelController) -> Router {
     Router::new()
@@ -16,30 +19,32 @@ pub fn routes(mc: ModelController) -> Router {
 
 async fn create_ticket(
     State(mc): State<ModelController>,
+    ctx: Ctx,
     Json(ticket_fc): Json<TicketForCreate>,
 ) -> Result<Json<Ticket>> {
     println!("->> {:<12} - create_ticket", "HANDLER");
 
-    let ticket = mc.create_ticket(ticket_fc).await?;
+    let ticket = mc.create_ticket(ctx, ticket_fc).await?;
 
     Ok(Json(ticket))
 }
 
-async fn list_tickets(State(mc): State<ModelController>) -> Result<Json<Vec<Ticket>>> {
+async fn list_tickets(State(mc): State<ModelController>, ctx: Ctx) -> Result<Json<Vec<Ticket>>> {
     println!("->> {:<12} - list_tickets", "HANDLER");
 
-    let tickets = mc.list_tickets().await?;
+    let tickets = mc.list_tickets(ctx).await?;
 
     Ok(Json(tickets))
 }
 
 async fn delete_ticket(
     State(mc): State<ModelController>,
+    ctx: Ctx,
     Path(id): Path<u64>,
 ) -> Result<Json<Ticket>> {
     println!(">>> {:<15} - delete_tickets", "HANDLER");
 
-    let ticket = mc.delete_ticket(id).await?;
+    let ticket = mc.delete_ticket(ctx, id).await?;
 
     Ok(Json(ticket))
 }
