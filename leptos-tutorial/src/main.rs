65fc338989d6d9e3b2b7fc9e92896ba2d235ce27
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-06-29 22:37:37 +0200

    Leptos tutorial: Component Props

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -6,27 +6,32 @@ fn main() {
 }
 
 #[component]
+fn ProgressBar(progress: ReadSignal<i32>) -> impl IntoView {
+    view! {
+        <progress
+            max="50"
+            value=progress
+        />
+    }
+}
+
+#[component]
 fn App() -> impl IntoView {
     let (x, set_x) = create_signal(0);
     let (p, set_p) = create_signal(0);
-
-    // Create a derived signal to monitor the multiplied value of `p`.
-    // Derived signals let you create reactive computed values that can be used in multiple places in your application with minimal overhead.
-    // This ensures the reactivity system correctly tracks changes to `p` and updates the derived signal accordingly.
-    let p_multiplied = move || p() * 3;
 
     view! {
         <button
             on:click=move |_| {
                 set_x.update(|n| *n += 10);
             }
-            // Set the 'style' attribute
+
             style="position: absolute"
-            // and toggle individual CSS properties with 'style:'
+
             style:left=move || format!("{}px", x() + 100)
             style:background-color=move || format!("rgb({}, {}, 100)", x(), 100)
             style:max-width="400px"
-            // Set a CSS variable for stylesheet use
+
             style=("--columns", x)
         >
             "Click to move"
@@ -40,16 +45,7 @@ fn App() -> impl IntoView {
             "Click to progress"
         </button>
         <br />
-        // Display the raw `p` value for debugging
-        <p>{move || format!("p value: {}", p())}</p>
-        // Display the multiplied `p` value for debugging
-        <p>{move || format!("p multiplied value: {}", p_multiplied())}</p>
-        <progress
-            max="50"
-            // Use the derived signal `p_multiplied` for the `value` attribute
-            // When move || p() * 3 was used, the progress bar filling speed didn't change,
-            // regardless of however p() was multiplied
-            value=move || p_multiplied()
-        />
+
+        <ProgressBar progress=p/>
     }
 }
