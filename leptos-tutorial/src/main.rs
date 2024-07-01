1a2fd7379f1e8dc4fd8c963052735792ca9a5ec7
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2024-07-01 17:59:24 +0200

    13x13 board without hoshis

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -7,22 +7,26 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let vector = vec![0, 1, 2];
+    // Define the positions and starting points for the lines
+    let y_positions: Vec<_> = (1..=260).step_by(20).collect();
+    let x_positions = y_positions.clone();
 
+    // Create the horizontal lines using a loop
+    let horizontal_lines = y_positions.into_iter().map(|y| {
+        view! {
+            <line x1="0" y1={y.to_string()} x2="242" y2={y.to_string()} stroke="black" stroke-width="2" />
+        }
+    }).collect_view();
+    let vertical_lines = x_positions.into_iter().map(|x| {
+        view! {
+            <line x1={x.to_string()} y1="0" x2={x.to_string()} y2="242" stroke="black" stroke-width="2" />
+        }
+    }).collect_view();
+
+    // Render the SVG element with the lines
     view! {
-        <p>{format!("A line drawn with SVG:")}</p>
-        <br />
-        <svg height="300" width="300" xmlns="http://www.w3.org/2000/svg">
-            <line x1="0" x2="300" y1="0" y2="0" style="stroke:red;stroke-width:2" />
+        <svg width="260" height="260" xmlns="http://www.w3.org/2000/svg">
+            {[horizontal_lines, vertical_lines]}
         </svg>
-        <p>{format!("A Vec:")}</p>
-        <p>{vector.clone()}</p>
-        <p>{format!("Vec elements one by one:")}</p>
-        <ul>
-            {vector.into_iter()
-                .map(|n| view! {<li>{n}</li>})
-                .collect::<Vec::<_>>()
-            }
-        </ul>
     }
 }
