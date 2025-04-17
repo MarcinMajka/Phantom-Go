6905cd15b5dcb999ba1e268521c28e9a1676d3bc
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-04-17 16:01:23 +0200

    Refactor: moved elements{}, updateTurn() and updateCaptures() to UI.js

---
+++ frontend/UI.js
@@ -0,0 +1,26 @@
+export const elements = {
+  turn: document.getElementById("player-turn"),
+  captures: {
+    black: document.getElementById("black-captures"),
+    white: document.getElementById("white-captures"),
+  },
+  addStone: {
+    black: document.getElementById("black-stone-button"),
+    white: document.getElementById("white-stone-button"),
+  },
+  removeStone: document.getElementById("remove-stone-button"),
+};
+
+export function updateTurn(currentPlayer) {
+  if (currentPlayer === "counting") {
+    elements.turn.innerText = "Counting points";
+    countingPhase = true;
+  } else {
+    elements.turn.innerText = "Turn: " + currentPlayer;
+  }
+}
+
+export function updateCaptures(blackCaptures, whiteCaptures) {
+  elements.captures.black.innerText = "Black Captures: " + blackCaptures;
+  elements.captures.white.innerText = "White Captures: " + whiteCaptures;
+}
--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,5 @@
+import { elements, updateTurn, updateCaptures } from "./UI.js";
+
 import {
   getBoardSVG,
   addBackground,
@@ -7,19 +9,6 @@ import {
   cellSize,
   padding,
 } from "./utils.js";
-
-const elements = {
-  turn: document.getElementById("player-turn"),
-  captures: {
-    black: document.getElementById("black-captures"),
-    white: document.getElementById("white-captures"),
-  },
-  addStone: {
-    black: document.getElementById("black-stone-button"),
-    white: document.getElementById("white-stone-button"),
-  },
-  removeStone: document.getElementById("remove-stone-button"),
-};
 
 let svg;
 let svgBlackPlayerBoard, svgWhitePlayerBoard;
@@ -387,18 +376,4 @@ function updateBoard(boardState) {
   for (const stone of whiteStonesAdded) {
     addGuessStone("white", ...stone);
   }
-}
-
-function updateTurn(currentPlayer) {
-  if (currentPlayer === "counting") {
-    elements.turn.innerText = "Counting points";
-    countingPhase = true;
-  } else {
-    elements.turn.innerText = "Turn: " + currentPlayer;
-  }
-}
-
-function updateCaptures(blackCaptures, whiteCaptures) {
-  elements.captures.black.innerText = "Black Captures: " + blackCaptures;
-  elements.captures.white.innerText = "White Captures: " + whiteCaptures;
 }
