e4902305cc7943b8c61acf2ca6cb3bffba149c9b
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-07-29 23:31:29 +0200

    Refactor: made the test more proper

--- frontend/tests/board.spec.ts
+++ frontend/tests/board.spec.ts
@@ -1,16 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5501/frontend/index.html');
-  await page.getByRole('textbox', { name: 'Enter match string' }).fill(';hlmlykmnglhkmnglhlwkmsnglhk');
+test('Change in guess stones in quick succession', async ({ page }) => {
+  await page.goto('/frontend/index.html');
+  await page.locator('#match-string').fill(';hlmlykmnglhkmnglhlwkmsnglhk');
   await page.locator('button').click();
   await page.locator('#guess-stone-button').click();
   await page.locator('#remove-stone-button').click();
 
-  for (let i = 0; i < 2 * 1000; i++) {
-    console.log(`Iteration: ${i}`);
-    await page.locator('circle:nth-child(130)').click();
-    await page.locator('.stone').click(); 
+  const guessStone = page.locator('.stone');
+  const emptyField = page.locator('circle:nth-child(130)');
+
+  for (let i = 0; i < 100; i++) {
+    await emptyField.click();
+    expect(guessStone).toBeVisible();
+    await guessStone.click();
+    expect(emptyField).toBeVisible();
   }
-  
 });
