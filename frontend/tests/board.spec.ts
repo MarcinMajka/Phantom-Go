ecd574cd1b4815402988fe15c43ca87f8099e615
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-08-03 22:12:28 +0200

    Tests: created 'Throttling' describe block

--- frontend/tests/board.spec.ts
+++ frontend/tests/board.spec.ts
@@ -45,40 +45,42 @@ test("Add then remove all guess stones in quick succession", async ({ page }) =>
   await expect(guessStones).toHaveCount(0, { timeout: 5000 });
 });
 
-test("Throttled - 3G: Add then remove all guess stones in quick succession", async ({ context, page }) => {
-    // Setting a longer timeout for this test
-    test.setTimeout(120000);
+test.describe('Throttling', () => {
+    test("3G: Add then remove all guess stones in quick succession", async ({ context, page }) => {
+        // Setting a longer timeout for this test
+        test.setTimeout(120000);
 
-    // Initiate throttling with Chrome DevTools Protocol
-    const cdpSession = await context.newCDPSession(page);
-    await cdpSession.send('Network.emulateNetworkConditions', NETWORK_PRESETS.Regular3G);
+        // Initiate throttling with Chrome DevTools Protocol
+        const cdpSession = await context.newCDPSession(page);
+        await cdpSession.send('Network.emulateNetworkConditions', NETWORK_PRESETS.Regular3G);
 
-  await page.goto('/frontend/index.html');
-  await page.locator('#match-string').fill(generateMatchID());
-  await page.locator('button').click();
+        await page.goto('/frontend/index.html');
+        await page.locator('#match-string').fill(generateMatchID());
+        await page.locator('button').click();
 
-  await page.waitForLoadState('networkidle');
+        await page.waitForLoadState('networkidle');
 
-  await page.locator('#guess-stone-button').click(); 
+        await page.locator('#guess-stone-button').click(); 
 
-  for (let i = 33; i <= 202; i++) {
-    await page.locator(`circle:nth-child(${i})`).click();
-  }
-  
-  const guessStones = page.locator('.stone');
-  const count = await guessStones.count();
+        for (let i = 33; i <= 202; i++) {
+            await page.locator(`circle:nth-child(${i})`).click();
+        }
+        
+        const guessStones = page.locator('.stone');
+        const count = await guessStones.count();
 
-  for (let i = 0; i < count; i++) {
-    expect(guessStones.nth(i)).toBeVisible();
-  }
+        for (let i = 0; i < count; i++) {
+            expect(guessStones.nth(i)).toBeVisible();
+        }
 
-  await page.locator('#remove-stone-button').click();
-  
-  // Change to decrementing for checking each change
-  for (let i = count; i > 0; i--) {
-    await guessStones.last().click();
-    await expect(guessStones).toHaveCount(i - 1);
-  }
+        await page.locator('#remove-stone-button').click();
+        
+        // Change to decrementing for checking each change
+        for (let i = count; i > 0; i--) {
+            await guessStones.last().click();
+            await expect(guessStones).toHaveCount(i - 1);
+        }
+    });
 });
 
 function generateMatchID() {
