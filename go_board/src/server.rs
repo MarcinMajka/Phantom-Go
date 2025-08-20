8769819f4eb973247d46edcbb22d841837a87171
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2025-08-20 23:54:23 +0200

    Added a TODO

--- go_board/src/server.rs
+++ go_board/src/server.rs
@@ -672,6 +672,9 @@ async fn sync_boards(payload: Json<SyncBoardsPayload>) -> Result<Json<GameState>
         .entry(payload.match_string.clone())
         .or_insert((Vec::new(), Vec::new()));
 
+    // TODO: Bug! Spectator first opens a game, without a seated player and unwraps a None value (player)
+    println!("{}", payload.player);
+
     let game_state = match room.board.get_winner() {
         Some(winner) => GameState::new(
             format!("Game over! Winner: {}", winner.to_string()),
