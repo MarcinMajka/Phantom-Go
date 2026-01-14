fc79755b1ce8f83059850b1587dffb6e5c632598
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2026-01-14 20:11:27 +0100

    Show games in a list

--- frontend/games.js
+++ frontend/games.js
@@ -1,5 +1,15 @@
 import { getGamesList } from "./handlers.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  console.log(await getGamesList());
+  const games = await getGamesList();
+
+  const gamesPanel = document.getElementById("games-panel");
+  const ul = document.createElement("ul");
+  gamesPanel.append(ul);
+
+  for (const game of games) {
+    const li = document.createElement("li");
+    li.textContent = game;
+    ul.appendChild(li);
+  }
 });
