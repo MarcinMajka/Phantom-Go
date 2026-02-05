e9526c11cdc2c4bdd550867b55d88c892c0974f3
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2026-02-05 22:10:22 +0100

    Tests & Refactor: POM - implemented class LoginPage

--- frontend/tests/pages/LoginPage.ts
+++ frontend/tests/pages/LoginPage.ts
@@ -1,1 +1,20 @@
 import { Page } from "@playwright/test";
+
+export class LoginPage {
+  constructor(readonly page: Page) {}
+
+  async goto() {
+    await this.page.goto("/frontend/index.html");
+  }
+
+  async joinGame(matchString: string) {
+    await this.page.locator("#match-string").fill(matchString);
+    await this.page.locator("#join-button").click();
+  }
+
+  async joinGameAsSpectator(matchString: string) {
+    await this.page.locator("#match-string").fill(matchString);
+    await this.page.locator("#spectator-checkbox").click();
+    await this.page.locator("#join-button").click();
+  }
+}
