22b92ba96e245f51d00d982f0d0c92d257a8630d
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2026-02-09 23:21:37 +0100

    Tests & Refactor: POM - updated class PlayerPage

--- frontend/tests/pages/PlayerPage.ts
+++ frontend/tests/pages/PlayerPage.ts
@@ -3,8 +3,40 @@ import { Page, Locator, expect } from "playwright/test";
 export class PlayerPage {
   constructor(readonly page: Page) {}
 
+  get playerTitle() {
+    return this.page.locator("#player-title");
+  }
+
+  get stonesInAtari() {
+    return this.page.locator("#stones-in-atari");
+  }
+
+  get playerTurn(): Locator {
+    return this.page.locator("#player-turn");
+  }
+
+  get blackCaptures(): Locator {
+    return this.page.locator("#black-captures");
+  }
+
   get whiteCaptures(): Locator {
     return this.page.locator("#white-captures");
+  }
+
+  get undoButton(): Locator {
+    return this.page.locator("#undo-button");
+  }
+
+  get passButton(): Locator {
+    return this.page.locator("#pass-button");
+  }
+
+  get guessStoneButton(): Locator {
+    return this.page.locator("#guess-stone-button");
+  }
+
+  get resignButton(): Locator {
+    return this.page.locator("#resign-button");
   }
 
   async clickAtCoordinate(x: number, y: number) {
@@ -13,5 +45,21 @@ export class PlayerPage {
     );
 
     await intersection.click();
+  }
+
+  async clickUndo() {
+    await this.undoButton.click();
+  }
+
+  async clickPass() {
+    await this.passButton.click();
+  }
+
+  async clickGuessStoneButton() {
+    await this.guessStoneButton.click();
+  }
+
+  async clickResign() {
+    await this.resignButton.click();
   }
 }
