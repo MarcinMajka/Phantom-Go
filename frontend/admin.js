8f41d99785e83dfdfd48de35abb4951cc9501f3c
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2026-04-16 18:17:46 +0200

    Refactor: cleanup

--- frontend/admin.js
+++ frontend/admin.js
@@ -36,36 +36,7 @@ const resetButton = createButton("admin-button", "Reset backend memory", () => {
 document.getElementById("admin-panel").appendChild(resetButton);
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const games = await getGamesList();document.addEventListener("DOMContentLoaded", async () => {
-    const games = await getGamesList();
-  
-    const gamesPanel = document.getElementById("games-panel");
-    const ul = document.createElement("ul");
-    gamesPanel.append(ul);
-  
-    for (const game of games) {
-      const li = document.createElement("li");
-      li.textContent = game;
-      li.onclick = () => {
-        fetchWithErrorHandling(`${API_URL}/validate-spectator`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            match_string: game,
-            session_token: getPlayerSessionToken(),
-          }),
-        }).then((data) => {
-          console.log(data);
-          const href = data.redirect_url + data.session_token;
-          console.log(href);
-          window.location.href = href;
-        });
-      };
-      ul.appendChild(li);
-    }
-  })
+  const games = await getGamesList();
 
   const gamesPanel = document.getElementById("games-panel");
   const ul = document.createElement("ul");
