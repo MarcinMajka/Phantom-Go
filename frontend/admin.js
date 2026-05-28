4f593f9b36b0d2b282bbea7c4c3571e4689ffdfc
MarcinMajka <86927565+MarcinMajka@users.noreply.github.com>
2026-05-28 23:09:19 +0200

    Refactor: removed a finished TODO

--- frontend/admin.js
+++ frontend/admin.js
@@ -12,7 +12,6 @@ console.log("You opened the Admin Panel!");
 const API_URL = getAPIUrl();
 
 // TODO: investigate how to make this panel password protected
-// TODO: show list of active games, with game status
 
 const resetButton = createButton("admin-button", "Reset backend memory", () => {
   fetch(`${API_URL}/reset-memory`, {
